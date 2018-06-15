92add194753f59ecd57f93c0032d8ba01747f020
JackEdwardLyons <jack.edward.lyons@gmail.com>
2018-06-15 22:34:11 +1000

    CityList View Add range slider and create mixin to store slider data

--- src/components/city_search/CityList.vue
+++ src/components/city_search/CityList.vue
@@ -1,6 +1,23 @@
 <template>
     <section>
         <h1>City Search App</h1>
+        <hr>
+        <ul class="flex alphabet-index basic-list">
+          <li v-for ="letter in alphabet" :key="letter">
+            {{ letter }}
+          </li>
+        </ul>
+        <hr>
+        <div class="filter-options">
+          <vue-slider v-model="sliderValue" :max="1000" v-bind="options"></vue-slider>
+          Filter options to go here
+          1. sort by population <br>
+          2. sort by rank <br>
+          3. name Search <br>
+          4. sliding scale ( population / rank ) -- if 'filter by pop is selected, then change to population slider' <br>
+          5. slider needs to be dynamic based on what cities are available.
+
+        </div>
         <ul class="box-list">
           <li v-for="city in cities" :key="city.rank">
             {{ city.city }}
@@ -16,11 +33,30 @@
 </template>
 
 <script>
+/**
+ * TODO:
+ * - Sort by A - Z , Filter by Name , Rank , Population ( use buttons that allow multi filter options )
+ * TODO:
+ * - Add cool animations to items that get filtered/sorted
+ * TODO:
+ * - Lazy load 50 at a time ? load more on scroll ....
+*/
+
 import { get } from 'vuex-pathify'
+import { rangeSlider } from '../../mixins/rangeSlider.js'
 
 export default {
+  mixins: [ rangeSlider ],
+  data () {
+    return {
+      sliderValue: [0, 500]
+    }
+  },
   computed: {
-    cities: get('citiesModule/cities')
+    cities: get('citiesModule/cities'),
+    alphabet () {
+      return 'abcdefghijklmnopqrstuvwxyz'.toUpperCase().split('')
+    }
   },
   methods: {
     numberWithCommas (x) {
@@ -32,7 +68,7 @@ export default {
   }
 }
 </script>
-<style scoped>
+<style lang="scss" scoped>
 hr {
   width: 50%;
   margin: 1em auto;
@@ -40,5 +76,12 @@ hr {
 }
 .stats-card {
   text-align: left;
+}
+.alphabet-index {
+  font-weight: bold;
+}
+.basic-list {
+  list-style-type: none;
+  padding: 0;
 }
 </style>
---
+++ src/mixins/rangeSlider.js
@@ -0,0 +1,29 @@
+import vueSlider from 'vue-slider-component'
+
+export const rangeSlider = {
+  components: {
+    vueSlider
+  },
+  data () {
+    return {
+      options: {
+        width: '90%',
+        dotSize: 20,
+        min: 0,
+        max: 1000,
+        disabled: false,
+        show: true,
+        tooltip: 'always',
+        tooltipDir: ['bottom', 'top'],
+        piecewise: true,
+        style: {
+          'marginBottom': '30px',
+          'marginLeft': 'auto',
+          'marginRight': 'auto'
+        },
+        lazy: true,
+        interval: 100
+      }
+    }
+  }
+}
